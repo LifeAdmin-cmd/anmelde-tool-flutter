2379717835500f2db54d9dad4284a2cf67e1ffb3
Lars <lars.kra2001@gmail.com>
2023-09-23 15:55:26 +0900

    added person selection on personen_form.dart

--- lib/screens/personen_form.dart
+++ lib/screens/personen_form.dart
@@ -13,13 +13,98 @@ class PersonenForm extends StatefulWidget {
 }
 
 class _PersonenFormState extends State<PersonenForm> {
-  List<Map<String, dynamic>> persons = []; // Updated to Map<String, dynamic>
+  List<Map<String, dynamic>> persons = [];
+  List<Map<String, dynamic>> registeredPersons = [];
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Column(
         children: [
+          const Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Column(
+              children: [
+                Text(
+                  "Deine Personen",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+                Divider(indent: 50.0, endIndent: 50.0,),
+                Text("Hier werden alle von dir gespeicherten Personen angezeigt", textAlign: TextAlign.center,),
+                Divider(indent: 50.0, endIndent: 50.0,),
+              ],
+            ),
+          ),
+
+          // Angemeldete Personen
+          const Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Text(
+              "Angemeldete Personen",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14.0,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: registeredPersons.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      children: [
+                        Text('${registeredPersons[index]['firstName']} ${registeredPersons[index]['lastName']}'),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () async {
+                                final result = await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => NewPersonForm(
+                                      initialValues: registeredPersons[index],
+                                      genders: widget.genders,
+                                      eatingHabits: widget.eatingHabits,
+                                    ),
+                                  ),
+                                );
+                                if (result is Map<String, dynamic>) {
+                                  setState(() {
+                                    registeredPersons[index] = result;
+                                  });
+                                }
+                              },
+                              child: Text('Bearbeiten'),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  persons.add(registeredPersons[index]);
+                                  registeredPersons.removeAt(index);
+                                });
+                              },
+                              child: Text('Entfernen'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+
+          // gespeicherte personen
+          const Divider(indent: 50.0, endIndent: 50.0,),
           ElevatedButton(
             onPressed: () async {
               final result = await Navigator.push(
@@ -37,33 +122,56 @@ class _PersonenFormState extends State<PersonenForm> {
                 });
               }
             },
-            child: Text('Add New Person'),
+            child: const Text('Neue Person'),
           ),
+          const SizedBox(height: 12.0,),
           Expanded(
             child: ListView.builder(
               itemCount: persons.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('${persons[index]['firstName']} ${persons[index]['lastName']}'),
-                  trailing: ElevatedButton(
-                    onPressed: () async {
-                      final result = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => NewPersonForm(
-                            initialValues: persons[index],
-                            genders: widget.genders,
-                            eatingHabits: widget.eatingHabits,
-                          ),
+                return Card(
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      children: [
+                        Text('${persons[index]['firstName']} ${persons[index]['lastName']}'),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () async {
+                                final result = await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => NewPersonForm(
+                                      initialValues: persons[index],
+                                      genders: widget.genders,
+                                      eatingHabits: widget.eatingHabits,
+                                    ),
+                                  ),
+                                );
+                                if (result is Map<String, dynamic>) {
+                                  setState(() {
+                                    persons[index] = result;
+                                  });
+                                }
+                              },
+                              child: Text('Bearbeiten'),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  registeredPersons.add(persons[index]);
+                                  persons.removeAt(index);
+                                });
+                              },
+                              child: Text('Hinzufügen'),
+                            ),
+                          ],
                         ),
-                      );
-                      if (result is Map<String, dynamic>) {
-                        setState(() {
-                          persons[index] = result;
-                        });
-                      }
-                    },
-                    child: Text('Edit'),
+                      ],
+                    ),
                   ),
                 );
               },
@@ -74,6 +182,7 @@ class _PersonenFormState extends State<PersonenForm> {
     );
   }
 }
+
 
 class NewPersonForm extends StatefulWidget {
   final Map<String, dynamic>? initialValues;
@@ -106,7 +215,7 @@ class _NewPersonFormState extends State<NewPersonForm> {
                 const BuchstabenInput(labelText: "Fahrtenname", idName: "scoutName", required: false,),
                 const DateTimeInput(labelText: "Geburtsdatum", idName: "birthday"),
                 DropdownInput(labelText: "Geschlecht", idName: "gender", data: widget.genders),
-                ChoiceInput(labelText: "Essgewohnheiten", idName: "eatingHabits", data: widget.eatingHabits, required: false,),
+                ChoiceInput(labelText: "Essensbesonderheiten", idName: "eatingHabits", data: widget.eatingHabits, required: false,),
                 const BuchstabenInput(labelText: "Straße und Hausnummer", idName: "adress", regex: r"",),
                 const BuchstabenInput(labelText: "Postleitzahl", idName: 'plz', regex: r'\b\d+\b', regexError: "Eine PLZ kann nur aus Zahlen bestehen",),
                 const SizedBox(height: 12.0,),
